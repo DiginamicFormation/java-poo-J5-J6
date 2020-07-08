eef8b152279452d55e0e77b48dab16aeb485fb20
rbonnamy@diginamic.fr <RichardBONNAMY@DESKTOP-K9SOQ13>
2020-07-08 14:03:16 +0200

    xx

--- src/main/java/sets/TestSetDouble.java
+++ src/main/java/sets/TestSetDouble.java
@@ -1,31 +1,40 @@
 package sets;
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
+/**
+ * 
+ */
 public class TestSetDouble {
+
 	public static void main(String[] args) {
-		// Liste doubles: 1.5, 8.25, -7.32, 13.3, -12.45, 48.5, 0.01
-		Set<Double> set = new HashSet<>();
-		set.add(1.5);
-		set.add(8.25);
-		set.add(-7.32);
-		set.add(13.3);
-		set.add(-12.45);
-		set.add(48.5);
-		set.add(0.01);
-		
-		System.out.println("Avant suppression :" + set);
-		
-		Iterator<Double> iterator = set.iterator();
-		
-		Double min = Double.MAX_VALUE;
-		while (iterator.hasNext()) {
-			Double n = iterator.next();
-			if (n < min){
-				min = n;
-			}
+		HashSet<Double> set = new HashSet<>();
+		set.addAll(Arrays.asList(1.5, 8.25, -7.32, 13.3, -12.45, 48.5, 0.01));
+
+		// Affichez tous les éléments de la collection
+		for (Double valeur : set) {
+			System.out.print(valeur + " ");
 		}
+		System.out.println();
+		System.out.println("--------------------------------------------------");
+
+		// Recherchez le plus grand élément de la collection
+		double max = Collections.max(set); // plus petite valeur des Double
+		double min = Collections.min(set); // plus grande valeur des Double
+
+		System.out.println("Mon maximum est : " + max);
+		System.out.println("Mon minimum est : " + min);
+
+		// Supprimez le plus petit élément de la collection
 		set.remove(min);
-		
-		System.out.println("Après suppression :" + set);
-		
+
+		System.out.println("--------------------------------------------------");
+		// Affichez enfin le contenu de la collection ainsi modifiée
+		for (Double valeur : set) {
+			System.out.print(valeur + " ");
+		}
 	}
+
 }
